2e4c4589f0c43df8fa23da488880f19c61f10a8b
Sam Wilson <swilson0702@gmail.com>
2025-09-11 23:33:22 -0500

    Produce the first image

--- main.cpp
+++ main.cpp
@@ -1,25 +1,57 @@
 #include <iostream>
 
 #include <color.hpp>
-#include <vec3.hpp>
+#include <ostream>
 #include <ray.hpp>
+#include <vec3.hpp>
 
-static constexpr int IMAGE_WIDTH = 256;
-static constexpr int IMAGE_HEIGHT = 256;
+auto ray_color(ray const &r) -> color {
+  static constexpr color white{1.0, 1.0, 1.0};
+  static constexpr color blue{0.5, 0.7, 1.0};
+
+  vec3 unit_direction = unit_vector(r.direction());
+  auto a = 0.5 * (unit_direction.y + 1.0);
+  return (1.0 - a) * white + (a * blue);
+}
 
 int main() {
-    std::cout << "P3\n" << IMAGE_WIDTH << " " << IMAGE_HEIGHT << "\n255\n";
+  // Image
+  auto aspect_ratio = 16.0 / 9.0;
+  int image_width = 400;
+  int image_height = std::max(static_cast<int>(image_width / aspect_ratio), 1);
 
-    for (int j = 0; j < IMAGE_HEIGHT; j++) {
-        std::clog << "\rScanlines remaining: " << (IMAGE_HEIGHT - j) << ' ' << std::flush;
-        for (int i = 0; i < IMAGE_WIDTH; i++) {
-            auto r = static_cast<double>(i) / static_cast<double>(IMAGE_WIDTH - 1);
-            auto g = static_cast<double>(j) / static_cast<double>(IMAGE_HEIGHT - 1);
-            auto b = 0.0;
+  // Camera
+  auto focal_length = 1.0;
+  auto viewport_height = 2.0;
+  auto viewport_width = viewport_height * (static_cast<double>(image_width) /
+                                           static_cast<double>(image_height));
+  auto camera_center = point3{0.0, 0.0, 0.0};
 
-            auto pixel_color = color{r, g, b};
-            write_color(std::cout, pixel_color);
-        }
+  auto viewport_u = vec3{viewport_width, 0.0, 0.0};
+  auto viewport_v = vec3{0.0, -viewport_height, 0.0};
+
+  auto pixel_delta_u = viewport_u / image_width;
+  auto pixel_delta_v = viewport_v / image_height;
+
+  auto viewport_upper_left = camera_center - vec3{0, 0, focal_length} -
+                             (viewport_u / 2) - (viewport_v / 2);
+  auto pixel00_loc =
+      viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
+
+  std::cout << "P3\n" << image_width << " " << image_height << "\n255\n";
+
+  for (int j = 0; j < image_height; j++) {
+    std::clog << "\rScanlines remaining: " << (image_height - j) << ' '
+              << std::flush;
+    for (int i = 0; i < image_width; i++) {
+      auto pixel_center =
+          pixel00_loc + (i * pixel_delta_u) + (j * pixel_delta_v);
+      auto ray_direction = pixel_center - camera_center;
+      ray r(camera_center, ray_direction);
+
+      auto pixel_color = ray_color(r);
+      write_color(std::cout, pixel_color);
     }
-    std::clog << "\rDone.                          \n";
+  }
+  std::clog << "\rDone.                          \n";
 }
